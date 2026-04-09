8af4d06ee10baac1870083a98f8d23541bc9e795
Rohit Sharma <111916295+RohitClass@users.noreply.github.com>
2026-04-10 01:39:51 +0530

    Initial commit

---
+++ src/main/java/com/batchly/batchly/BatchlyApplication.java
@@ -0,0 +1,13 @@
+package com.batchly.batchly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchlyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BatchlyApplication.class, args);
+	}
+
+}
